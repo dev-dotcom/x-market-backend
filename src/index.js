083fd8da1772a437b94157067df666ab39ed7ea6
Dev Faldu <devfadadu118@gmail.com>
2024-06-30 19:36:38 +0100

    express app setup in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import app from "./app.js";
 import dbConnection from "./db/connection.js";
 import dotenv from "dotenv";
 
@@ -6,3 +7,8 @@ dotenv.config({
 })
 
 dbConnection()
+.then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`App is listening on port ${process.env.PORT}`)
+  })
+})
