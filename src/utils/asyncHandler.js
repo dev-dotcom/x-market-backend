318f49b3638a9b50f29f868cae0c5be655900c68
Dev Faldu <devfadadu118@gmail.com>
2024-07-08 18:55:19 +0100

    wip

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -15,7 +15,7 @@ export {asyncHandler}
 // with promises
 
 const asyncHandlerWithPromises = (func) => {
-    (req,res,next) => {
+    return (req,res,next) => {
         Promise.resolve(func(req,res,next)).catch((error) => next(err))
     } 
 }
