01985d245cb29364e3b231cb7181fcbf21736ca2
dev <dev.f@hashtechy.com>
2024-07-08 18:31:14 +0530

    path error solved

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import app from "./app.js";
 import dbConnection from "./db/connection.js";
 import dotenv from "dotenv";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const env = process.env.NODE_ENV || 'development';
 const envFilePath = path.resolve(__dirname, `.env.${env}`);
-dotenv.config({path: envFilePath})
+dotenv.config({ path: envFilePath });
+
+console.log(process.env.NODE_ENV);
+
 
 dbConnection()
 .then((response) => {
